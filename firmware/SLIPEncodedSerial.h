78a438a12c500700a92c0ccfe9eb0f9cb2f62885
jg33 <freelancer@Freelancers-iMac.local>
2014-07-18 19:23:33 -0400

    removed print.h refs

--- firmware/SLIPEncodedSerial.h
+++ firmware/SLIPEncodedSerial.h
@@ -7,7 +7,7 @@ Extends the Serial class to encode SLIP over serial
 #if ARDUINO >= 100
 #include "Arduino.h"
 #else
-#include "WProgram.h"
+//#include "WProgram.h"
 #endif
 #include <Stream.h>
 #include <HardwareSerial.h>
